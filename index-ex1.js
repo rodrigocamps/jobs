5fa14bf3d3b8ae89b81e53a94072b63929fb3646
rodrigocamps <50743035+rodrigocamps@users.noreply.github.com>
2019-09-30 18:26:46 -0300

    Ejercicio 1
    Examen Artear rcamps

---
+++ index-ex1.js
@@ -0,0 +1,11 @@
+const getResponseObject = () => {
+ 	({
+ 	"1a": "This is an exercise.",
+ 	"1b": "You must fix the error",
+ 	"1c": "and make a PR."
+ 	});
+ 	};
+ 	 
+ 	const main = () => Object.values(getResponseObject()).join(" ");
+ 	 
+ 	console.log(main());
